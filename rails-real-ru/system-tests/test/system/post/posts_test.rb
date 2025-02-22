1b5afb32c65ec27e63e4fd023c016b0627845112
@hexlet/cli <support@hexlet.io>
2025-02-22 21:32:45 +0100

    submit rails-real-ru/system-tests

---
+++ rails-real-ru/system-tests/test/system/post/posts_test.rb
@@ -0,0 +1,45 @@
+require "application_system_test_case"
+
+class PostsTest < ApplicationSystemTestCase
+  setup do
+    @post = posts(:one) # Предполагается, что у вас есть фикстуры или фабрики для постов
+  end
+
+  test "создание поста" do
+    visit posts_url
+    click_on "New Post"
+
+    fill_in "Title", with: "Новый пост"
+    fill_in "Body", with: "Это новый пост"
+    click_on "Create Post"
+
+    assert_text "Post was successfully created."
+  end
+
+  test "просмотр поста" do
+    visit post_url(@post)
+
+    assert_text @post.title
+    assert_text @post.body
+  end
+
+  test "редактирование поста" do
+    visit edit_post_url(@post)
+
+    fill_in "Title", with: "Измененный заголовок"
+    fill_in "Body", with: "Измененный текст"
+    click_on "Update Post"
+
+    assert_text "Post was successfully updated."
+  end
+
+  test "удаление поста" do
+    visit posts_url
+
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Post was successfully destroyed."
+  end
+end
